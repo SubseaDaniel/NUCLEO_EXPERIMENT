8c8a262e2efd532f1caac253aafea4c8438f4533
Daniel Vedernikov <d.vedernikov@subsea-europe.com>
2025-07-25 10:26:04 +0200

    achieved the ability to debug *YAY*

--- CM4/Core/Src/main.c
+++ CM4/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "fatfs.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -105,6 +106,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_SDMMC1_SD_Init();
+  MX_FATFS_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
